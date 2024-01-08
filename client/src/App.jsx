f64adabc1731c960b1c4f7dafd52dd808907772c
Ybarvaliya <uvlbarvaliya@gmail.com>
2024-01-08 23:29:49 +0530

    basic installtions

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      <div className="bg-black text-white text-xl text-center p-5">
+        PROPERTY MARKETPLACE
+      </div>
+      <div className="text-center">
+        DO WHATEVER YOU LIKE !!
+      </div>
+    </>
+  )
+}
+
+export default App
