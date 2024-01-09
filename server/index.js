7c6288a4f275ab40d89acc6bdc07d5aa77aaca87
Ybarvaliya <uvlbarvaliya@gmail.com>
2024-01-09 09:08:57 +0530

    backend installtions

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const cors = require('cors')
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv'
 
-const app = express()
+dotenv.config()
 
-app.use(cors())
+const app = express();
 
-app.listen(5555, () => {
-    console.log('server running on 5555')
-})
+const PORT = process.env.PORT || 5555
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
